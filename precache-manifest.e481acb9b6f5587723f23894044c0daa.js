8eaafa2af3f2490d53a4e1825fc652fde5a9bc2b
mhd_faizan <agile.faizan@gmail.com>
2020-06-10 09:57:26 +0500

    Updates

---
+++ precache-manifest.e481acb9b6f5587723f23894044c0daa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256c44320739a7efd3709c097a646a51",
+    "url": "/react-todo-github-demo/index.html"
+  },
+  {
+    "revision": "b89b5576746a98369bcd",
+    "url": "/react-todo-github-demo/static/css/main.1a0873a2.chunk.css"
+  },
+  {
+    "revision": "2a2e1f84d026968e1dc5",
+    "url": "/react-todo-github-demo/static/js/2.f5d8c1ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-github-demo/static/js/2.f5d8c1ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b89b5576746a98369bcd",
+    "url": "/react-todo-github-demo/static/js/main.7311229b.chunk.js"
+  },
+  {
+    "revision": "e1b42d8f4382e9cbe908",
+    "url": "/react-todo-github-demo/static/js/runtime-main.92334053.js"
+  }
+]);
